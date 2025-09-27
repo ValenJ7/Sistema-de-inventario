4e23a6a19dea1a2bf85212a3871009efb514b0f9
ValenJ7 <valentinjuarez733@gmail.com>
2025-09-27 00:43:29 -0300

    vacio

--- frontend/src/features/products/components/ui/LocalProductImages.jsx
+++ frontend/src/features/products/components/ui/LocalProductImages.jsx
@@ -9,7 +9,7 @@ export default function LocalProductImages({ value = [], onChange }) {
   // notificar a padre
   useEffect(() => {
     onChange?.(items.map((x, i) => ({ ...x, sort_order: i + 1 })));
-  }, [items]); // eslint-disable-line
+  }, [items]); // eslint-disable-lineee
 
   function handlePick() {
     inputRef.current?.click();
